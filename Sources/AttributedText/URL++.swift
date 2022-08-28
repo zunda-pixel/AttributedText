2023b8fed6e02f594c7ebe30eb16d30b28bb009c
zunda <47569369+zunda-pixel@users.noreply.github.com>
2022-08-28 23:57:40 +0900

    URLからqueryItemsを取得できるExtensionを追加

---
+++ Sources/AttributedText/URL++.swift
@@ -0,0 +1,14 @@
+//
+// URL++.swift
+//
+
+
+import Foundation
+
+extension URL {
+  var queryItems: [URLQueryItem] {
+    guard let urlComponents: URLComponents = .init(url: self, resolvingAgainstBaseURL: true) else { return [] }
+
+    return urlComponents.queryItems ?? []
+  }
+}
