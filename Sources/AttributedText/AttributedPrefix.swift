36e006cb8c74399c204488beab5550057b84878c
zunda <47569369+zunda-pixel@users.noreply.github.com>
2022-09-06 01:04:03 +0900

    public initを追加

--- Sources/AttributedText/AttributedPrefix.swift
+++ Sources/AttributedText/AttributedPrefix.swift
@@ -7,4 +7,9 @@ import Foundation
 public struct AttributedPrefix {
   let prefix: String
   let container: AttributeContainer
+
+  public init(prefix: String, container: AttributeContainer) {
+    self.prefix = prefix
+    self.container = container
+  }
 }
