511e77860a539e8155a28b16c48471d5f3399915
zunda <47569369+zunda-pixel@users.noreply.github.com>
2022-09-05 23:54:45 +0900

    .environment(\.openURL, OpenURLAction)でopenURLのアクションを独自実装するように

--- Sources/AttributedText/AttributedText.swift
+++ Sources/AttributedText/AttributedText.swift
@@ -7,13 +7,43 @@ import SwiftUI
 import RegexBuilder
 
 public struct AttributedText: View {
-  public init(text: String, prefixes: [String : AttributeContainer], urlContainer: AttributeContainer) {
-    self.attributedText = AttributedString(text).addingURL(container: urlContainer).addingPrefixLink(prefixes: prefixes)
+  static let attributedURL = URL(string: "urlForAttributedText://")!
+
+  let prefixes: [String]
+
+  let action: (String, String) -> Void
+
+  public init(text: String, prefixes: [AttributedPrefix], urlContainer: AttributeContainer, action: @escaping (String, String) -> Void ) {
+    self.prefixes = prefixes.map(\.prefix)
+
+    self.attributedText = AttributedString(text)
+                            .addingURL(container: urlContainer)
+                            .addingPrefixLink(prefixes: prefixes)
+
+    self.action = action
   }
 
   let attributedText: AttributedString
 
   public var body: some View {
     Text(attributedText)
+      .environment(\.openURL, OpenURLAction { url in
+        guard url.scheme == AttributedText.attributedURL.scheme else {
+          return .systemAction(url)
+        }
+
+        let query = url.queryItems.first { $0.name == "query" }!.value!
+
+        let prefix = prefixes.first {
+          let prefix = $0
+
+          guard prefix.startIndex <= query.endIndex && prefix.endIndex <= query.endIndex  else { return false }
+          return prefix == query[prefix.startIndex..<prefix.endIndex]
+        }!
+
+        action(prefix, query)
+
+        return .handled
+      })
   }
 }
