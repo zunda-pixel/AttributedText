74a163c1897048356a1817d2335827a1d1611c68
zunda <zunda.dev@gmail.com>
2023-02-08 17:15:05 +0900

    add support macCatalyst

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
   name: "AttributedText",
-  platforms: [.iOS(.v16), .macOS(.v13), .watchOS(.v9), .tvOS(.v16)],
+  platforms: [.iOS(.v16), .macOS(.v13), .watchOS(.v9), .tvOS(.v16), .macCatalyst(.v16)],
   products: [
     // Products define the executables and libraries a package produces, and make them visible to other packages.
     .library(
